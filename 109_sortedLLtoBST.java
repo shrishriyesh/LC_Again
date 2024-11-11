23c655cb885899627a2cc650ce30f3b83b23d58e
Shriyesh <shriyesh.chandra@gmail.com>
2024-11-11 15:49:50 -0500

    DIvide from Mid and recurse left and right

---
+++ 109_sortedLLtoBST.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        // Finding the middle element
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+         TreeNode root = new TreeNode(slow.val);
+         // Disconnect the left part of the list
+        // fast = slow.next;
+        if (prev != null) {
+            prev.next = null;
+        }
+
+        // Recursively build left and right subtrees
+        root.left = sortedListToBST(head);   // Left subtree
+        root.right = sortedListToBST(slow.next);  // Right subtree
+
+        return root;
+    }
+}
