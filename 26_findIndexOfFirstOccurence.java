58b5d4f8d473f71cc8970dbc68deaee22d5baf62
Shriyesh <shriyesh.chandra@gmail.com>
2025-01-11 22:29:42 +0530

    Substring search

---
+++ 26_findIndexOfFirstOccurence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i=0;i<haystack.length()-needle.length()+1;i++)
+        {
+            if(needle.equals(haystack.substring(i,needle.length()+i)))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
