ad2dfcbd4cd7a3ae69deaabc0b472c9918eff77f
Shriyesh <shriyesh.chandra@gmail.com>
2025-02-25 17:13:36 -0500

    Another way of doing the same thing, used a prev pointer and also a temp pointer that gets returned

--- 2_addTwoNumbers.java
+++ 2_addTwoNumbers.java
@@ -48,3 +48,43 @@ class Solution {
         return head.next;
     }
 }
+
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry=0;
+        ListNode temp=l1;
+        ListNode prev=null;
+        while(l1!=null&&l2!=null)
+        {
+            int sum=l1.val+l2.val+carry;
+            l1.val=sum%10;
+            carry=sum/10;
+            prev=l1;
+            l1=l1.next;
+            l2=l2.next;
+        }
+        while(l1!=null)
+        {
+            int sum=l1.val+carry;
+            l1.val=sum%10;
+            prev=l1;
+            carry=sum/10;
+            l1=l1.next;
+        }
+        prev.next=l2;
+        while(l2!=null)
+        {
+            int sum=l2.val+carry;
+            l2.val=sum%10;
+            prev=l2;
+            carry=sum/10;
+            l2=l2.next;
+        }
+        if(carry!=0)
+        {
+            l1.next=new ListNode(carry);
+        }
+        return temp;
+    }
+}
