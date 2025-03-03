48f0c8b0787a4727fb7ffe7ef7a62079cf813267
Shriyesh <shriyesh.chandra@gmail.com>
2025-03-03 03:16:56 -0500

    Redid the question

--- 1004_MaximumConsecutive1sIII.java
+++ 1004_MaximumConsecutive1sIII.java
@@ -28,3 +28,25 @@ class Solution {
         return maxc;
     }
 }
+
+
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//         int l=0,r=0;
+//         int ans=0;
+//         int zero=0;
+//         while(r<nums.length){
+//             if(nums[r]==0){
+//                 zero++;
+//             }
+//             while(zero>k){
+//                 if(nums[l]==0)
+//                     zero--;
+//                 l++;
+//             }
+//             ans=Math.max(ans,r-l+1);
+//             r++;
+//         }
+//         return ans;
+//     }
+// }
