aadfd5410caffd2e5ea3718350ce9e2279e942e0
Shriyesh <shriyesh.chandra@gmail.com>
2025-03-04 20:00:16 -0500

    Redid it with DFS

--- 547_numberOfProvinces.java
+++ 547_numberOfProvinces.java
@@ -22,3 +22,31 @@ class Solution {
         return count;
     }
 }
+
+
+//// Approach 2: DFS
+
+// public int findCircleNum(int[][] isConnected) {
+//     int n = isConnected.length;
+//     boolean[] visited = new boolean[n];
+//     int provinces = 0;
+    
+//     for (int i = 0; i < n; i++) {
+//         if (!visited[i]) { // Only start a DFS if the node hasn't been visited
+//             dfs(isConnected, visited, i);
+//             provinces++;
+//         }
+//     }
+    
+//     return provinces;
+// }
+
+// private void dfs(int[][] isConnected, boolean[] visited, int city) {
+//     visited[city] = true;
+//     for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+//         if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
+//             dfs(isConnected, visited, neighbor);
+//         }
+//     }
+// }
+// }
