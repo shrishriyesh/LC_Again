be385798e67d1e33e4a266bfb7a5a81576ec1d3e
Shriyesh <shriyesh.chandra@gmail.com>
2025-03-13 21:15:13 -0400

    Stack based approach for Iterative Inorder Traversal of a Binary Tree

--- 173_BSTIterator.java
+++ 173_BSTIterator.java
@@ -49,3 +49,59 @@ class BSTIterator {
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
+
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    Stack<TreeNode> st;
+    public BSTIterator(TreeNode root) {
+        st=new Stack<>();
+        while(root!=null)
+        {
+            st.push(root);
+            root=root.left;
+        }
+    }
+    
+    public int next() {
+        TreeNode temp=st.pop();
+        int res=temp.val;
+        if(temp.right!=null)
+        {
+            st.push(temp.right);
+            while(temp.right.left!=null)
+            {
+                st.push(temp.right.left);
+                temp.right=temp.right.left;
+            }
+        }
+        return res;
+        
+    }
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
