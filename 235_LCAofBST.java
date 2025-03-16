42d5b1ea2493ac12dfde937ab292c7c15d62e274
Shriyesh <shriyesh.chandra@gmail.com>
2025-03-15 20:24:16 -0400

    Better 100% speed

--- 235_LCAofBST.java
+++ 235_LCAofBST.java
@@ -22,3 +22,43 @@ class Solution {
         return left;
     }
 }
+
+// Simple traversal using BST
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//  class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root==null)
+//         return root;
+//         int first;
+//         int sec;
+//         if(p.val<q.val)
+//         {
+//             first=p.val;
+//             sec=q.val;
+//         }
+//         else
+//         {
+//             first=q.val;
+//             sec=p.val;
+//         }
+//         while(true)
+//         {
+//             if(first==root.val||sec==root.val|| (first<root.val && sec>root.val))
+//             return root;
+//             if(first<root.val && sec<root.val)
+//             root=root.left;
+//             else
+//             root=root.right;
+//         }
+//     }
+// }
