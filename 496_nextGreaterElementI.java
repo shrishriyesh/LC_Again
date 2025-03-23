acb5c9afe6f3fa9066cc042f256663fe25a4f583
Shriyesh <shriyesh.chandra@gmail.com>
2025-03-23 19:57:58 -0400

    Another method to do this question

--- 496_nextGreaterElementI.java
+++ 496_nextGreaterElementI.java
@@ -40,3 +40,53 @@ class Solution {
         return res;
     }
 }
+
+
+
+//2nd approach
+//Checked all the elements of nums2 to see if stack has a greater element or not
+//If not then push the element in stack
+//If yes then pop the element from stack and check if it is present in nums1 or not
+//If present then assign the value to the result array
+//If not present then pop the element from stack
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n = nums2.length;
+        int[] result = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0;i<nums1.length;i++)
+        {
+            if(!map.containsKey(nums1[i]))
+            {
+                map.put(nums1[i],i);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            while(!stack.isEmpty() && stack.peek() < nums2[i])
+            {   if(map.containsKey(stack.peek()))
+                {
+                    int index = map.get(stack.pop());
+                    result[index] = nums2[i];
+                }
+                else
+                {
+                    //result[map.get(stack.peek())] = -1;
+                    stack.pop();
+                }
+            }
+            if(map.containsKey(nums2[i]))
+            {
+                stack.push(nums2[i]);
+            }
+        }
+        while(!stack.isEmpty())
+        {
+            int index = map.get(stack.pop());
+            result[index] = -1;
+        }
+        return result;
+    }
+}
