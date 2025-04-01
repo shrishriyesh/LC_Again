d9023a0a4777e86ee57dc80bad57554df2e07c50
Shriyesh <shriyesh.chandra@gmail.com>
2025-04-01 02:21:37 -0400

    Used the height traversal method

--- 1650_lowestCommonAncestorOfBinaryTree.java
+++ 1650_lowestCommonAncestorOfBinaryTree.java
@@ -29,4 +29,55 @@ class Solution {
         }
         return q;
     }
+
+    
+}
+
+/*
+Another method,
+Time complexity: O(h) where h is the height of the tree
+Space complexity: O(1) as we are using constant space
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        int hp=height(p);
+        int hq= height(q);
+        Node temp=null;
+        if(hp<hq)
+        {
+           while(hq!=hp)
+           {
+            q=q.parent;
+            hq--;
+           }
+        }
+        else
+        {
+           while(hq!=hp)
+           {
+            p=p.parent;
+            hp--;
+           }
+        }
+        while(p!=q)
+        {
+            p=p.parent;
+            q=q.parent;
+        }
+        return p;
+        
+
+    }
+    public int height(Node x)
+    {
+        int h=1;
+        while(x.parent!=null)
+        {
+            x=x.parent;
+            h++;
+        }
+        return h;
+    }
 }
