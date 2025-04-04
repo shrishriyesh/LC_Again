7457905dc5fb3164cdc9bc5f2f4dcfdfa32c9c39
Shriyesh <shriyesh.chandra@gmail.com>
2025-04-03 23:36:49 -0400

    Used a Hashset to store the numbers in the array and then iterated through the set to see the longest seq

---
+++ 128_longestConsecutiveSequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int maxLength = 0;
+        for(int curr:set)
+        {
+            if(!set.contains(curr-1))
+            {
+                int currentNum = curr;
+                int currentLength = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLength++;
+                }
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+        return maxLength;
+    }
+}
