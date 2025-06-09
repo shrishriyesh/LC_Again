ae7611af8eeed29ac357515b18ee22f622661335
Shriyesh <shriyesh.chandra@gmail.com>
2025-06-09 16:26:55 -0400

    Can detect cycles using 3 maps for Topsort approach

--- 3481_applySubstitutions.java
+++ 3481_applySubstitutions.java
@@ -47,3 +47,92 @@ class Solution {
         return expanded;
     }
 }
+// TopSort Approach:
+
+class Solution {
+    public String applySubstitutions(List<List<String>> replacements, String text) {
+        
+        HashMap<String, String> replacementMap = new HashMap<>();
+
+        Map<String, List<String>> graph = new HashMap<>();
+
+        Map<String, Integer> indegreeMap = new HashMap<>();
+
+        for (List<String>replacement : replacements){
+            String key = replacement.get(0);
+            String value = replacement.get(1);
+
+            int i = 0;
+            graph.putIfAbsent(key, new ArrayList<>());
+            indegreeMap.put(key, 0);
+            while (i< value.length()){
+                if (value.charAt(i) != '%'){
+                    i += 1;
+                }else{
+                    // collecting all the dependecies. for eg. abc%B%de%A%
+                    i += 1;
+                    String neighborKey = String.valueOf(value.charAt(i));
+                    graph.putIfAbsent(neighborKey, new ArrayList<>());
+                    graph.get(neighborKey).add(key);
+                    indegreeMap.put(key, indegreeMap.get(key)+1);
+                    i += 2;
+                    
+                }
+            }
+            replacementMap.put(key, value);
+        }
+
+        Queue<String> q = new LinkedList<>();
+
+        for (String key : indegreeMap.keySet()){
+            if (indegreeMap.get(key) == 0){
+                q.add(key);
+            }
+        }
+
+        while (q.size() > 0){
+            String curr = q.poll();
+
+            StringBuilder sb = new StringBuilder();
+            String currRes = replacementMap.get(curr);
+
+            int i = 0;
+            while (i < currRes.length()){
+                if (currRes.charAt(i) != '%'){
+                    sb.append(currRes.charAt(i));
+                    i += 1;
+                }else{
+                    // getting the values of all the dependencies for the curr key.
+                    i += 1;
+                    sb.append(replacementMap.get(String.valueOf(currRes.charAt(i))));
+                    i += 2;
+                }
+            }
+            replacementMap.put(curr, sb.toString());
+
+            for (String neighbor : graph.get(curr)){
+                indegreeMap.put(neighbor, indegreeMap.get(neighbor)-1);
+                if (indegreeMap.get(neighbor) == 0){
+                    q.add(neighbor);
+                }
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        int i = 0;
+        while (i< text.length()){
+            if (text.charAt(i) != '%'){
+                ans.append(text.charAt(i));
+                i += 1;
+            }else{
+                i += 1;
+                String key = String.valueOf(text.charAt(i));
+                ans.append(replacementMap.get(key));
+                i += 2;
+            }
+        }
+
+        return ans.toString();
+    }
+}
